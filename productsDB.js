933bc70f199f5db2470c9cc25105e55d921534ea
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-01-29 20:10:53 +0200

    Add products database with initial product listings

---
+++ productsDB.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: "Cotton T-Shirt",
+    image: "https://m.media-amazon.com/images/I/41wv06V2zLL._AC_SX679_.jpg",
+    price: 19.99,
+    stock: 5
+  },
+  {
+    id: 2,
+    name: "Wireless Headphones",
+    image: "https://m.media-amazon.com/images/I/6169m-oKFEL._AC_SL1500_.jpg",
+    price: 149.99,
+    stock: 3
+  },
+  {
+    id: 3,
+    name: "Stainless Steel Water Bottle",
+    image: "https://m.media-amazon.com/images/I/41zWqqEVoTL._AC_SL1042_.jpg",
+    price: 24.95,
+    stock: 7
+  },
+  {
+    id: 4,
+    name: "Premium Coffee Mug",
+    image: "https://m.media-amazon.com/images/I/41Th5lIpiLL._AC_SL1024_.jpg",
+    price: 12.50,
+    stock: 8
+  },
+  {
+    id: 5,
+    name: "Ultra HD Smart TV",
+    image: "https://m.media-amazon.com/images/I/71dX2O72HXL._AC_SL1500_.jpg",
+    price: 799.99,
+    stock: 2
+  },
+  {
+    id: 6,
+    name: "Wireless Bluetooth Speaker",
+    image: "https://m.media-amazon.com/images/I/71DRh3h61OL._AC_SL1500_.jpg",
+    price: 89.99,
+    stock: 6
+  },
+  {
+    id: 7,
+    name: "Running Shoes",
+    image: "https://m.media-amazon.com/images/I/61+d8jOEGBL._AC_SY625_.jpg",
+    price: 129.95,
+    stock: 4
+  },
+  {
+    id: 8,
+    name: "Gaming Laptop",
+    image: "https://m.media-amazon.com/images/I/61wFLokn9RL._AC_SL1000_.jpg",
+    price: 1599.00,
+    stock: 1
+  },
+  {
+    id: 9,
+    name: "Smartwatch Pro",
+    image: "https://m.media-amazon.com/images/I/7185QN-PcSL._AC_SL1500_.jpg",
+    price: 299.99,
+    stock: 9
+  },
+  {
+    id: 10,
+    name: "Wireless Keyboard",
+    image: "https://m.media-amazon.com/images/I/61kSyd-2kgL._AC_SL1500_.jpg",
+    price: 59.95,
+    stock: 10
+  }
+];
+
+export default products;
